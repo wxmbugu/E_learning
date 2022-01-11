d9fe815049e549501c63bd0825ecf92e6e38c577
Wambugu <54021365+Wambug@users.noreply.github.com>
2022-01-11 18:24:50 +0300

    Update section_test.go

--- controllers/section_test.go
+++ controllers/section_test.go
@@ -24,8 +24,7 @@ func TestFindSetion(t *testing.T) {
 		fmt.Println(args.Name)
 		require.NoError(t, err)
 		require.NotNil(t, result)
-		_, err = FindSection(context.Background(), "", "ok")
-		require.Error(t, err)
+		
 	}
 
 }
