a1b80eb32f22bba41d9eaeb8889017b03714df37
Wambug <wambugumacharia35@gmail.com>
2022-02-16 21:46:32 +0300

    bcyrpt

--- util/password.go
+++ util/password.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HashPassword(password string) (string, error) {
-	hashpassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	hashpassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", fmt.Errorf("%s: %w", "Couldn't hash password", err)
 	}
